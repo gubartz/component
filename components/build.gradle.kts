025ea4afd810f92cc05d8314e606f6c60ea403c7
gubartz <gubartz@gmail.com>
2026-03-20 14:14:53 -0300

    teste2

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -70,7 +70,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.gubartz"
                 artifactId = "component"
-                version = "1.0.2"
+                version = "1.0.4"
             }
         }
     }
